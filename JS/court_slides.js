0c85eefe6a1ebc3a4f47d9b9794bb6174a37fe59
remkfer <remkfer@gmail.com>
2023-12-14 18:16:32 +0800

    initial

---
+++ JS/court_slides.js
@@ -0,0 +1,16 @@
+const courtlist = [...document.querySelectorAll('.courtlist')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+courtlist.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
